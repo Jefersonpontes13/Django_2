919148d7780728fb4046852b8131ec5f3399866c
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-05-18 17:03:20 -0300

    ok

--- core/views.py
+++ core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', template_name='index')
+    return render(request, 'index.html')
 
 
 def contato(request):
-    return render(request, 'contato.html', template_name='contato')
+    return render(request, 'contato.html')
 
 
 def produto(request):
-    return render(request, 'produto.html', template_name='produto')
+    return render(request, 'produto.html')
