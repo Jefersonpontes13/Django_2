0046a6b56b3efd739d09c9cedf6cf447e8751fb8
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-05-19 20:24:59 -0300

    ProdutoViewForm Ok

--- Django_2/settings.py
+++ Django_2/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
